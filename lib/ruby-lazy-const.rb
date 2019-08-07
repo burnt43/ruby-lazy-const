aec2d5aafc7d3f5f90fe08ffb506d81d272ac305
burnt43 <jms.crsn@gmail.com>
2019-08-07 12:49:45 -0400

    added basic stuff

---
+++ lib/ruby-lazy-const.rb
@@ -0,0 +1,36 @@
+require 'pathname'
+
+class Object
+  CONST_MISSING_DEFINITION =
+    proc do |missing_const_name|
+      possible_sub_pathname =
+        if self == Object
+          Pathname.new('.')
+        else
+          Pathname.new(name.split('::').map(&:underscore).join('/'))
+        end
+
+      possible_pathname =
+        Pathname.new(File.expand_path(__FILE__)).parent
+        .join(possible_sub_pathname)
+        .join("#{missing_const_name.to_s.underscore}.rb")
+
+      if possible_pathname.exist?
+        require possible_pathname
+
+        if const_defined?(missing_const_name)
+          constant = const_get(missing_const_name)
+
+          constant.define_singleton_method(:const_missing, CONST_MISSING_DEFINITION)
+
+          constant
+        else
+          super(missing_const_name)
+        end
+      else
+        super(missing_const_name)
+      end
+    end
+
+  define_singleton_method(:const_missing, CONST_MISSING_DEFINITION)
+end
---
+++ ruby-lazy-const.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'ruby-lazy-const'
+  s.version     = '0.0.1'
+  s.summary     = 'ruby consts load lazily with const_missing; const namespace match directory structure.'
+  s.description = 'Lazily Load Consts'
+  s.authors     = ['James Carson']
+  s.email       = 'jms.crsn@gmail.com'
+  s.homepage    = "http://tmpurl.com"
+  s.files       = ['lib/ruby-lazy-const.rb']
+  s.license     = 'MIT'
+end
