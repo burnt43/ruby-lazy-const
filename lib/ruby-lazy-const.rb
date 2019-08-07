1eb16478101f00b721c52b35806d0b01e86abe85
burnt43 <jms.crsn@gmail.com>
2019-08-07 16:10:29 -0400

    remove Ruby from the const names. you know what language this is.

--- lib/ruby-lazy-const.rb
+++ lib/ruby-lazy-const.rb
@@ -1,7 +1,7 @@
 require 'pathname'
 require 'active_support'
 
-module RubyLazyConst
+module LazyConst
   class Config
     class << self
       attr_accessor :base_dir
@@ -20,7 +20,7 @@ class Object
         end
 
       possible_pathname =
-        Pathname.new(RubyLazyConst::Config.base_dir)
+        Pathname.new(LazyConst::Config.base_dir)
         .join(possible_sub_pathname)
         .join("#{ActiveSupport::Inflector.underscore(missing_const_name.to_s)}.rb")
 
